4f4334eec5b49ef584579d58fa15bc088587589f
Stan Saner <stan.saner@hotmail.co.uk>
2022-12-05 17:18:00 +0000

    Added questions array

---
+++ assets/js/questions.js
@@ -0,0 +1,32 @@
+var questions_array = [
+    {
+        title: 'What is the acronym for the Document Object Model?',
+        choices: ['JS', 'OBJ', 'DOM', 'ARR'],
+        answer: 'DOM'
+    },
+    {
+        title: 'The condition in an if / else statement is enclosed with _____',
+        choices: ['square brackets', 'parentheses', 'quotes', 'curly braces'],
+        answer: 'parentheses'
+    },
+    {
+        title: 'What type of for loop is used for iterating over an object\'s keys and values?',
+        choices: ['in', 'of', 'at', 'each'],
+        answer: 'in'
+    },
+    {
+        title: 'Commonly used data types do NOT include _____',
+        choices: ['strings', 'booleans', 'alerts', 'numbers'],
+        answer: 'alerts'
+    },
+    {
+        title: 'Arrays in Javascript can be used to store _____',
+        choices: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
+        answer: 'all of the above'
+    },
+    {
+        title: 'Strings must be enclosed within _____ when being assigned to variables',
+        choices: ['commas', 'curly braces', 'quotes', 'parentheses'],
+        answer: 'quotes'
+    }
+];
