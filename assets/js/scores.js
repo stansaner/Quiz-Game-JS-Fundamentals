cd2802f74195e6a08e5fc4c2b690d6b1715b64f3
Stan Saner <stan.saner@hotmail.co.uk>
2022-12-06 18:08:47 +0000

    Added scores displaying page handler

---
+++ assets/js/scores.js
@@ -0,0 +1,27 @@
+// Display High Scores of users
+
+var highScores = JSON.parse(localStorage.getItem('high_scores'));
+
+var scoresDisplay = document.querySelector('#highscores');
+
+
+// If the array is empty, don't bother
+if (highScores){
+    for (i = 0; i < highScores.length; i++ ) {
+    
+        scoresDisplay.insertAdjacentHTML('beforeend', `
+        <li>${highScores[i].initials}: ${highScores[i].score}</li>
+        `);
+    }
+}
+
+
+// Clear High Scores from the localStorage
+// when the "Clear High Scores" button is clicked
+
+var wipe = document.querySelector('#clear');
+
+wipe.addEventListener('click', function() {
+    localStorage.clear();
+    scoresDisplay.innerHTML = '';
+})
